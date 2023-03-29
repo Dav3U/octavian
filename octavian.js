f365580ee6f182a66660c7a41e35369803fbff39
Dave3U <davidujile@icloud.com>
2023-03-29 22:25:01 +0100

    added teechnical writing and media content

--- octavian.js
+++ octavian.js
@@ -1,42 +1,59 @@
 
 
-const links =document.querySelectorAll("a")
+// const links =document.querySelectorAll("a")
 
 
-document.getElementById("toggle").onclick = changeBack;
-const mode = document.getElementById("toggle")
+// document.getElementById("toggle").onclick = changeBack;
+// const mode = document.getElementById("toggle")
 
 
-function changeBack()
-{
-    document.getElementById("bodyy").classList.toggle('change');
-    document.body.classList.toggle('color');
+// function changeBack()
+// {
+//     document.getElementById("bodyy").classList.toggle('change');
+//     document.body.classList.toggle('color');
 
     
-    if (mode.innerHTML =="Toggle Dark Mode")
-    {
-    mode.innerHTML="Toggle Light Mode"
+//     if (mode.innerHTML =="Toggle Dark Mode")
+//     {
+//     mode.innerHTML="Toggle Light Mode"
 
-    }
+//     }
 
 
-    else if (mode.innerHTML =="Toggle Light Mode")
-    {
-    mode.innerHTML="Toggle Dark Mode"
+//     else if (mode.innerHTML =="Toggle Light Mode")
+//     {
+//     mode.innerHTML="Toggle Dark Mode"
 
-    }
+//     }
 
-    for (var i=0;i<=links.length;i++)
-    {
-        links[i].classList.toggle('color');
-     }
+//     for (var i=0;i<=links.length;i++)
+//     {
+//         links[i].classList.toggle('color');
+//      }
 
-}
+// }
 
-document.getElementById('modeIcon').addEventListener('click',()=>{
-    modeChange();  
-  })
+// document.getElementById('modeIcon').addEventListener('click',()=>{
+//     modeChange();  
+//   })
  
-   const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
-  "dark" :
-  "light";
+//    const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+//   "dark" :
+//   "light";
+
+
+
+  function changeOrder(x) {
+    if (x.matches) {
+
+        document.getElementById("thirdWriteup").classList.add('order-first')
+
+    } else {
+        document.getElementById("thirdWriteup").classList.remove('order-first')
+
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 992px)")
+  changeOrder(x) // Call listener function at run time
+  x.addListener(changeOrder) // Attach listener function on state changes

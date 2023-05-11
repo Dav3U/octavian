0fb8e48042335baec14a48f43a227b0a6d3dab1c
Dave3U <davidujile@icloud.com>
2023-05-11 04:14:41 +0100

    added mode toggle, resolved dead links

--- octavian.js
+++ octavian.js
@@ -1,45 +1,56 @@
+const html = document.getElementById('html')
+
+document.getElementById('modeIcon').addEventListener('click',()=>{
+  modeChange();  
+})
+
+ const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+"dark" :
+"light";
+
+// Set theme to the user's preferred color scheme
+function updateTheme() {
+const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+  "dark" :
+  "light";
+
+  if (colorMode=='light'){
+    document.getElementById('modeIcon').classList.remove('bi-brightness-high-fill')
+    document.getElementById('modeIcon').classList.add('bi-moon-stars-fill')
+  }
+  else{
+    document.getElementById('modeIcon').classList.add('bi-brightness-high-fill')
+    document.getElementById('modeIcon').classList.remove('bi-moon-stars-fill')
+  }
+  
+document.querySelector("html").setAttribute("data-bs-theme", colorMode);
+
+return;
+}
+
+// Set theme on load
+updateTheme()
+
+// Update theme when the preferred scheme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
 
 
-// const links =document.querySelectorAll("a")
+function modeChange(){
+  if (colorMode=='light'){
+    document.getElementById('modeIcon').classList.toggle('bi-brightness-high-fill');
+    document.getElementById('modeIcon').classList.toggle('bi-moon-stars-fill');
+  }
+  else{
+    document.getElementById('modeIcon').classList.toggle('bi-brightness-high-fill');
+    document.getElementById('modeIcon').classList.toggle('bi-moon-stars-fill');
+  }
 
-
-// document.getElementById("toggle").onclick = changeBack;
-// const mode = document.getElementById("toggle")
-
-
-// function changeBack()
-// {
-//     document.getElementById("bodyy").classList.toggle('change');
-//     document.body.classList.toggle('color');
-
-    
-//     if (mode.innerHTML =="Toggle Dark Mode")
-//     {
-//     mode.innerHTML="Toggle Light Mode"
-
-//     }
-
-
-//     else if (mode.innerHTML =="Toggle Light Mode")
-//     {
-//     mode.innerHTML="Toggle Dark Mode"
-
-//     }
-
-//     for (var i=0;i<=links.length;i++)
-//     {
-//         links[i].classList.toggle('color');
-//      }
-
-// }
-
-// document.getElementById('modeIcon').addEventListener('click',()=>{
-//     modeChange();  
-//   })
+  html.getAttribute('data-bs-theme')=='dark'? 
+  html.setAttribute('data-bs-theme','light') : 
+  html.setAttribute('data-bs-theme', 'dark')
  
-//    const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
-//   "dark" :
-//   "light";
+
+}
 
 
 
